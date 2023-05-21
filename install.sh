fe7796a5f0bc58b4a0bca4d668e8d8ef6cf6d35e
Eric Bäckhage <eric.backhage@gmail.com>
2023-05-21 09:45:36 +0200

    Added tmux conf

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ mkdir -p "$HOME/.config"
 # nvim
 rm -rf "$HOME/.config/nvim"
 ln -s "$HOME/Code/DotFiles/nvim" "$HOME/.config"
+
+# tmux
+mkdir -p "$HOME/.config/tmux"
+ln -sf "$HOME/Code/DotFiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
