d62b4d9138301e7a28bdbb7755a6e209d1a87a91
Eric Bäckhage <eric.backhage@gmail.com>
2023-05-26 16:54:18 +0200

    Add tmux plugin and theme

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ ln -s "$HOME/Code/DotFiles/nvim" "$HOME/.config"
 # tmux
 mkdir -p "$HOME/.config/tmux"
 ln -sf "$HOME/Code/DotFiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+rm -rf "$HOME/.tmux/plugins/tpm"
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
