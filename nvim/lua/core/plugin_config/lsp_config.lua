e7245788103e2ad0ae7a3394e75496a0dd83eb35
Eric Bäckhage <eric.backhage@gmail.com>
2023-05-26 16:44:53 +0200

    Add diagnostic keybindings

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -7,6 +7,8 @@ local on_attach = function(_, _)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
+    vim.keymap.set('n', 'gn', vim.diagnostic.goto_next, {})
+    vim.keymap.set('n', 'gp', vim.diagnostic.goto_prev, {})
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
     vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
