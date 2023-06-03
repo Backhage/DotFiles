5ff4917f231e7649fef0fcc682ca21f38c03888a
Eric Bäckhage <eric.backhage@gmail.com>
2023-06-03 22:59:11 +0200

    Adds null-ls and configures black formatter for Python

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -25,8 +25,12 @@ return require('packer').startup(function(use)
       'neovim/nvim-lspconfig',
   }
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+      'jose-elias-alvarez/null-ls.nvim',
+      requires = { { 'nvim-lua/plenary.nvim' } }
   }
   -- completion
   use 'hrsh7th/nvim-cmp'
