2807917df83269afd634f0ed50d387fbc1b8b125
Eric Bäckhage <eric.backhage@gmail.com>
2023-09-14 19:38:26 +0200

    Make TSUpdate run automatically

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -18,7 +18,13 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }
   use {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
