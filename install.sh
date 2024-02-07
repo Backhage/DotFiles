f7eb5daa73f9b6356f651fef048faefee6d652f7
Eric Bäckhage <eric.backhage@gmail.com>
2024-02-07 21:48:41 +0100

    Added note on how to install tpm plugins

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ rm -rf "$HOME/.config/nvim"
 ln -s "$HOME/Code/DotFiles/nvim" "$HOME/.config"
 
 # tmux
+# NOTE: After starting tmux, press "CTRL+<space> I" to install tpm plugins
 mkdir -p "$HOME/.config/tmux"
 ln -sf "$HOME/Code/DotFiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
 rm -rf "$HOME/.tmux/plugins/tpm"
