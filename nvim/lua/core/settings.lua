138789a7add08a3f4dd169947b9e9b8b862fa7a8
Eric Bäckhage <eric.backhage@gmail.com>
2024-02-18 14:19:23 +0100

    yank to unnamedplus

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -1,2 +1,5 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
+-- Set up clipboard to be able to yank and paste in other applications.
+-- Note: a clipboard handler needs to be installed. For Wayland, apt install wl-clipboard.
+vim.opt.clipboard:append { 'unnamedplus' }
