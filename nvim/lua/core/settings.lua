10f68a2b4b2404cfd8e26f6d7882de5cca0646e6
Eric Bäckhage <eric.backhage@gmail.com>
2024-05-07 21:45:12 +0200

    Turn off lsp logging

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -3,3 +3,5 @@ vim.wo.relativenumber = true
 -- Set up clipboard to be able to yank and paste in other applications.
 -- Note: a clipboard handler needs to be installed. For Wayland, apt install wl-clipboard.
 vim.opt.clipboard:append { 'unnamedplus' }
+-- Turn off lsp logging (can be re-enabled if needed)
+vim.lsp.set_log_level("off")
